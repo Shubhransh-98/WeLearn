a21f667720586fbee694e8289cdc3d8dd26bf180
Shubhransh-98 <72726736+Shubhransh-98@users.noreply.github.com>
2020-10-12 16:13:38 +0530

    Create 4.cpp

---
+++ 4.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"hello Shubhransh!"<<endl;
+    string s="SHUBHRANSH";
+    cout<<s;
+    int a=10;
+    cout<<a<<endl;
+    string t;
+    while(t!="exit")
+    {cout<<"ENTER PASS"<<endl;
+    cin>>t;
+    if(t==s)
+    {
+    cout<<t<<"correct"<<endl;
+    }
+    else
+    {
+        cout<<"Incorrect"<<endl;
+    }
+    }
+    
+}
