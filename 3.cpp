cea2504c99c3444af07536b2895d14807073185b
Shubhransh-98 <72726736+Shubhransh-98@users.noreply.github.com>
2020-10-12 16:12:47 +0530

    Create 3.cpp

---
+++ 3.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    string characterName="Carsel";
+    int characterAge;
+    characterAge=39;
+    cout<<"There was a man whose name was "<<characterName<<endl;
+    cout<<"He was "<<characterAge<<" years old"<<endl;
+    cout<<"He liked his name "<<characterName<<" alot"<<endl;
+    cout<<"But he was not happy that he was already "<<characterAge<<endl;
+    string characterName2;
+    cin>>characterName2;
+    int characterAge2;
+    cin>>characterAge2;
+   // characterAge2=39;
+    cout<<"There was a man whose name was "<<characterName2<<endl;
+    cout<<"He was "<<characterAge2<<" years old"<<endl;
+    cout<<"He liked his name "<<characterName2<<" alot"<<endl;
+    cout<<"But he was not happy that he was already "<<characterAge2<<endl;
+}
